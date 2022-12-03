48555512fa2f465c9914627e4f312e4348d83309
paul <paul@makovi.ch>
2022-12-03 15:15:13 +0300

    day 1 p1

--- src/bin/day01a.rs
+++ src/bin/day01a.rs
@@ -1,8 +1,14 @@
 pub fn main() {
-    let vs = include_str!("day01.input")
+    let max = include_str!("day01.input")
         .lines()
-        .map(|s| s.parse().unwrap())
-        .collect::<Vec<u16>>();
+        .map(|s| s.parse::<u32>().unwrap_or(0))
+        .fold((0, 0), |(max, cur), num| {
+            if num == 0 {
+                (max.max(cur), 0)
+            } else {
+                (max, cur + num)
+            }
+        });
 
-    dbg!(vs);
+    println!("{}", max.0);
 }
